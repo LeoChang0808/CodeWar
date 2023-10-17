a1eb16ef812138a7fa9c107b816fdf3f1df438ab
Leo Chang <leochang0808@gmail.com>
2023-10-17 17:18:41 +0800

    JavaScript(RGB To Hex Conversion)

--- codewar.js
+++ codewar.js
@@ -1,11 +1,18 @@
-function toCamelCase(str){
-  let strArr = str.split(/[_-]/)
-  let result = strArr[0]
-  for (let i = 1; i < strArr.length; i++) {
-    result += strArr[i][0].toUpperCase() + strArr[i].substring(1);
-  }
-  return result
+function rgb(r, g, b){
+  return [r,g,b].map( i => 
+    { return Math.min ( Math.max(i, 0), 255)
+      .toString(16).padStart(2,"0").toUpperCase()
+    }).join("")
 }
 
+console.log(rgb(0, 0, 0));
+// '000000'
 
-console.log(toCamelCase("the-stealth_warrior"));
+rgb(0, 0, -20)
+// '000000'
+
+rgb(300,255,255)
+// 'FFFFFF'
+
+rgb(173,255,47)
+// 'ADFF2F'
