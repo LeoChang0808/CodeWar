2e4ed6c02e0a9270cc7ee0adca9904f5a0aaf8d9
Leo Chang <leochang0808@gmail.com>
2023-10-17 16:00:18 +0800

    Ruby(Tribonacci Sequence)

--- codewar.rb
+++ codewar.rb
@@ -1,13 +1,31 @@
-def narcissistic?(value)
-  value == value.digits.map { |x| x ** value.digits.length}.sum
+def tribonacci(signature,n)
+  case
+  when signature.size == n
+    signature
+  when signature.size > n
+    signature.slice(0,n)
+  when signature.size < n
+    result = 0
+    for i in signature.size-3 .. signature.size-1
+      result += signature[i]
+    end
+    signature << result
+    tribonacci(signature,n)
+  end
 end
 
 
-narcissistic?(5)
-# true
+tribonacci([1,1,1],10)
+# [1,1,1,3,5,9,17,31,57,105]
 
-narcissistic?( 153 )
-# true
+tribonacci([0,0,1],10)
+# [0,0,1,1,2,4,7,13,24,44]
 
-narcissistic?( 1633 )
-# false
+tribonacci([0,1,1],10)
+# [0,1,1,2,4,7,13,24,44,81]
+
+tribonacci([1,0,0],10)
+# [1,0,0,1,1,2,4,7,13,24]
+
+tribonacci([0,0,0],10)
+# [0,0,0,0,0,0,0,0,0,0]
