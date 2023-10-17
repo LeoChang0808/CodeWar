0daf7ecc49e333df9fe33cb680548168ad6bdcf1
Leo Chang <leochang0808@gmail.com>
2023-10-17 16:20:55 +0800

    Ruby(Moving Zeros To The End)

--- codewar.rb
+++ codewar.rb
@@ -1,31 +1,11 @@
-def tribonacci(signature,n)
-  case
-  when signature.size == n
-    signature
-  when signature.size > n
-    signature.slice(0,n)
-  when signature.size < n
-    result = 0
-    for i in signature.size-3 .. signature.size-1
-      result += signature[i]
-    end
-    signature << result
-    tribonacci(signature,n)
+def moveZeros(arr)
+  zero = []
+  result = []
+  arr.each do |x|
+    x == 0 ? zero << 0 : result << x
   end
+  p result + zero
 end
 
-
-tribonacci([1,1,1],10)
-# [1,1,1,3,5,9,17,31,57,105]
-
-tribonacci([0,0,1],10)
-# [0,0,1,1,2,4,7,13,24,44]
-
-tribonacci([0,1,1],10)
-# [0,1,1,2,4,7,13,24,44,81]
-
-tribonacci([1,0,0],10)
-# [1,0,0,1,1,2,4,7,13,24]
-
-tribonacci([0,0,0],10)
-# [0,0,0,0,0,0,0,0,0,0]
+moveZeros([1,2,0,1,0,1,0,3,0,1])
+# [ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ]
