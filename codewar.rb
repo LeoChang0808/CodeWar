d8f292c82eff52a8addfff6ae29681928e620611
Leo Chang <leochang0808@gmail.com>
2023-10-17 16:46:46 +0800

    Ruby(Simple Pig Latin)

--- codewar.rb
+++ codewar.rb
@@ -1,11 +1,18 @@
-def moveZeros(arr)
-  zero = []
-  result = []
-  arr.each do |x|
-    x == 0 ? zero << 0 : result << x
-  end
-  p result + zero
+def pig_it text
+  text.split(" ").map{ | word |
+    if word.match?(/\p{Alpha}/)
+      word.slice(1..-1) + word[0] + "ay"
+    else
+      word
+    end
+  }.join(" ")
 end
 
-moveZeros([1,2,0,1,0,1,0,3,0,1])
-# [ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ]
+p pig_it('Pig latin is cool')
+# 'igPay atinlay siay oolcay'
+
+p pig_it('This is my string')
+# 'hisTay siay ymay tringsay'
+
+p pig_it('Quis custodiet ipsos custodes ?')
+# 'uisQay ustodietcay psosiay ustodescay ?'
