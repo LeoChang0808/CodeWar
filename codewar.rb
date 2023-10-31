286ac96d6ac79ca886895ea84864da4ce6458499
Leo Chang <leochang0808@gmail.com>
2023-10-31 09:58:26 +0800

    Ruby(Count IP Addresses)

--- codewar.rb
+++ codewar.rb
@@ -1,19 +1,9 @@
-def rgb(r, g, b)
-  number = [ r, g, b ].map do |x|
-    [0, [x, 255].min].max
-  end
-  result = number.map do |x|
-    x.to_s(16).rjust(2,"0").upcase()
-  end
-  result.join
+def ipsBetween(start, ending)
+  result = start.split(".").map{|n| n.to_i}.zip(ending.split(".").map{|n| n.to_i}).map{|x,y| x - y}
+  result.zip([256**3,256**2,256,1]).map{|x,y| x * y}.sum.abs
 end
 
-
-rgb(0, 0, 0)
-# '000000'
-rgb(0, 0, -20)
-# '000000'
-rgb(300,255,255)
-# 'FFFFFF'
-rgb(173,255,47)
-# 'ADFF2F'
+ipsBetween("10.0.0.0", "10.0.0.50")
+# 50
+ipsBetween("20.0.0.10", "20.0.1.0")
+# 246
