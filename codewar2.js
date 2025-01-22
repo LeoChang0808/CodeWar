b7ede7e4c29645379f5403a9a8dccca25fb6bbca
Leo Chang <leochang0808@gmail.com>
2025-01-22 17:30:34 +0800

    javascript(Highest Scoring Word)

--- codewar2.js
+++ codewar2.js
@@ -1,4 +1,20 @@
-function filter_list(l) {
-    return l.filter((text) => typeof(text) == "number" )
+function high(x) {
+    const alphabetArr = [...'abcdefghijklmnopqrstuvwxyz'];
+    const xArr = x.split(" ").map((word) => word.split(""));
+    let maxSum = 0
+    let maxWord = ""
+
+    for (let i = 0; i < xArr.length; i++) {
+        let wordSum = 0
+        xArr[i].map((t)=>{
+            wordSum += alphabetArr.indexOf(t)+1;
+        })
+        if (wordSum > maxSum) {
+            maxSum = wordSum
+            maxWord = xArr[i].join("")
+        }
+    }
+    return maxWord
 }
-console.log(filter_list([1,2,'a','b']));
+
+console.log(high('man i need a taxi up to ubud')); // 'taxi'
